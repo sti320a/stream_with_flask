85c4df09b48f1c136de30e1b5517586eb1f2217c
sti320a <sti320a@gmail.com>
2018-12-23 17:21:12 +0900

    Add video capture

--- camera.py
+++ camera.py
@@ -3,27 +3,46 @@
 # class Camera(object):
 #     def __init__(self):
 #         self.frames = [open('./static/img/' + f + '.jpg', 'rb').read() for f in ['1', '2', '3', '4', '5', '6', '7']]
+#         print(self.frames[0])
 
 #     def get_frame(self):
 #         return self.frames[int(time()) % 7]
 
-from time import time
+from time import time, sleep
 import cv2
 
 class Camera():    
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-    
+        # self.frames = [open('./static/img/' + f + '.jpg', 'rb').read() for f in ['1', '2', '3', '4', '5', '6', '7']]
+        self.frames = [open('./static/img/img' + num + '.jpg', 'rb').read() for num in ['1', '2', '3', '4', '5', '6', '7']]
+        
     def get_frame(self):
+        # self.open_camera()
         return self.frames[int(time()) % 7]
     
     def open_camera(self):
+
+        cnt = 0
+
         while True:
+            path = './static/img/img' + str(cnt) + '.jpg'
             ret, frame = self.cap.read()
+            
             cv2.imshow('Raw Frame', frame)
+            cv2.imwrite(path, frame)
+
+            cnt += 1
+
             k = cv2.waitKey(1)
             if k == 27:
                 break
+            
+            if cnt == 10:
+                break
+
+            sleep(1)
+
         self.cap.release()
         cv2.destroyAllWindows()
     
