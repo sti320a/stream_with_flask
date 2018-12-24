02bbcaf21122e65075abb6cd7dd8b4c042296d42
sti320a <sti320a@gmail.com>
2018-12-24 13:29:27 +0900

    Update

--- camera.py
+++ camera.py
@@ -1,54 +1,72 @@
-# from time import time
+from time import time
 
-# class Camera(object):
-#     def __init__(self):
-#         self.frames = [open('./static/img/' + f + '.jpg', 'rb').read() for f in ['1', '2', '3', '4', '5', '6', '7']]
-#         print(self.frames[0])
-
-#     def get_frame(self):
-#         return self.frames[int(time()) % 7]
-
-from time import time, sleep
-import cv2
-
-class Camera():
+class Camera(object):
     def __init__(self):
-        self.frames = [open('./static/img/img' + num + '.jpg', 'rb').read() for num in ['1', '2', '3', '4', '5', '6', '7']]
-        
+        self.frames = []
+        self.files = ['1', '2', '3', '4', '5']
+
+        for f in self.files:
+            self.frames.append(open('./static/img/' + f + '.jpg', 'rb').read())
+
     def get_frame(self):
-        # cnt = self.open_camera()
-        # self.frames = [open('./static/img/img' + str(f) + '.jpg', 'rb').read() for f in range(0,7)]
-        return self.frames[int(time()) % 7]
-    
-    def open_camera(self):
-        self.cap = cv2.VideoCapture(0)
-        
-        cnt = 0
+        return self.frames[int(time()) % len(self.files)]
 
-        while True:
-            path = './static/img/img' + str(cnt) + '.jpg'
-            ret, frame = self.cap.read()
-            
-            cv2.imshow('Raw Frame', frame)
-            cv2.imwrite(path, frame)
+# VideoCameraを追加
+class VideoCamera():
+    def __init__(self):
+        self.frames = []
+        self.files = ['1', '2', '3']
 
-            cnt += 1
+        for f in self.files:
+            self.frames.append(open('./static/video/' + f + '.avi', 'rb').read())
 
-            k = cv2.waitKey(1)
-            if k == 27:
-                break
-            
-            if cnt == 10:
-                break
+    def get_frame(self):
+        return self.frames[0]
 
-            sleep(1)
 
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-        return cnt    
-
-cam = Camera()
+cam = VideoCamera()
 
 if __name__=='__main__':
     cam.open_camera()
+
+
+# from time import time, sleep
+# import cv2
+
+# class Camera():
+#     def __init__(self):
+#         self.frames = [open('./static/img/img' + num + '.jpg', 'rb').read() for num in ['1', '2', '3', '4', '5', '6', '7']]
+        
+#     def get_frame(self):
+#         # cnt = self.open_camera()
+#         # self.frames = [open('./static/img/img' + str(f) + '.jpg', 'rb').read() for f in range(0,7)]
+#         return self.frames[int(time()) % 7]
+    
+#     def open_camera(self):
+#         self.cap = cv2.VideoCapture(0)
+        
+#         cnt = 0
+
+#         while True:
+#             path = './static/img/img' + str(cnt) + '.jpg'
+#             ret, frame = self.cap.read()
+            
+#             cv2.imshow('Raw Frame', frame)
+#             cv2.imwrite(path, frame)
+
+#             cnt += 1
+
+#             k = cv2.waitKey(1)
+#             if k == 27:
+#                 break
+            
+#             if cnt == 10:
+#                 break
+
+#             sleep(1)
+
+#         self.cap.release()
+#         cv2.destroyAllWindows()
+
+#         return cnt    
+
--- jpeg_stream.py
+++ jpeg_stream.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, Response
-from camera import Camera
+# from camera import Camera
+
+# VideoCameraに変更
+from camera import VideoCamera 
+from time import sleep
 
 app = Flask(__name__)
 
@@ -7,14 +11,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(camera):
+def generate(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/feed')
+def feed():
+    # VideoCameraに変更 
+    return Response(generate(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
